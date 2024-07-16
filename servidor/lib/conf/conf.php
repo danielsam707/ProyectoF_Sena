3cb2ed02ed66a2fc5d2641ca817529a29bc060cb
Cristian Castaño <81573985+CristianSk23@users.noreply.github.com>
2024-07-15 21:23:50 -0500

    Nuevos archivos

---
+++ servidor/lib/conf/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$database = "adso643";
+$pass = "";
+$port = "3306";
+
+?>
---
+++ servidor/model/MasterModel.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once "../lib/conf/connection.php";
+
+class MasterModel extends Connection{
+    public function insertar($sql){
+        $respuesta = mysqli_query($this->getConnect(),$sql);
+        return $respuesta;
+    }
+
+    public function consultar($sql){
+        $respuesta = mysqli_query($this->getConnect(),$sql);
+        return $respuesta;
+    }
+
+    public function editar($sql){
+        $respuesta = mysqli_query($this->getConnect(),$sql);
+        return $respuesta;
+    }
+
+    public function eliminar($sql){
+        $respuesta = mysqli_query($this->getConnect(),$sql);
+        return $respuesta;
+    }
+
+    public function autoincrement($table, $field){
+        $sql = "SELECT MAX($field) FROM $table";
+        $respuesta = $this->consultar($sql);
+        $contador = mysqli_fetch_row($respuesta);
+        return $contador[0]+1;
+    }
+}
+
+?>
